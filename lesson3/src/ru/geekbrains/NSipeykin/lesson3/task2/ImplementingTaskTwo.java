d6200a19657c67be685552f36d2a75f15c5539be
Nikita <Niksipeykin@mail.ru>
2020-12-26 04:03:19 +0300

    task one was copleted

--- lesson3/src/ru/geekbrains/NSipeykin/lesson3/task2/ImplementingTaskTwo.java
+++ lesson3/src/ru/geekbrains/NSipeykin/lesson3/task2/ImplementingTaskTwo.java
@@ -1,21 +1,11 @@
-package ru.geekbrains.NSipeykin.lesson3;
+package ru.geekbrains.NSipeykin.lesson3.task2;
 
-import ru.geekbrains.NSipeykin.lesson3.task1.ImplementingTaskOne;
-
-public class Main {
-
-//TODO:
-// 1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных
-// слов, из которых состоит массив (дубликаты не считаем). Посчитать сколько раз встречается каждое слово.
-
+public class ImplementingTaskTwo {
 //TODO:
 // 2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
 // В этот телефонный справочник с помощью метода add() можно добавлять записи. С помощью метода get() искать номер
 // телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
 // тогда при запросе такой фамилии должны выводиться все телефоны.
 
-    public static void main(String[] args) {
-        ImplementingTaskOne taskOne = new ImplementingTaskOne();
 
-    }
 }
---
+++ lesson3/src/ru/geekbrains/NSipeykin/lesson3/task2/PhoneDirectory.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.NSipeykin.lesson3.task2;
+
+import java.util.Map;
+
+public class PhoneDirectory {
+   // Map<String,Integer> phoneDirectory = new
+}
