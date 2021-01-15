2f45516f4a6ae04840146e19fe775ae6fc8e75e0
NikFamilyNB <niksipeykin@mail.ru>
2021-01-15 05:29:56 +0300

    lesson5 was completed

--- GBLesson5/src/ru/geekBrains/nikSipeykin/lesson5/Main.java
+++ GBLesson5/src/ru/geekBrains/nikSipeykin/lesson5/Main.java
@@ -66,42 +66,49 @@ public class Main {
 
     static void arr1(float[] arr){
 
-    arrCreator(arr);
+    arr = arrCreator(arr);
 
     long startTime = System.currentTimeMillis();
 
     arrCalculation(arr);
     long endTime = System.currentTimeMillis();
     long output = endTime - startTime;
-    System.out.println(output);
+    System.out.println("Array 1 was completed in "+ output + " milisec." );
 }
 
-    public static void main(String[] args) {
-//        arr1(arr);
-//        arr2(arr);
-        System.out.println(56/(3+4)*4);
-    }
-
     static void arr2(float[]arr){
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        arrCreator(arr);
+        Thread t1 = new Thread(() -> {
+            arrCalculation(a1);
+        });
+
+        Thread t2 = new Thread(() ->{
+            arrCalculation(a2);
+        });
+
         long startTime = System.currentTimeMillis();
-        float[] a1 = new float[size];
-        float[] a2 = new float[size];
+
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1f;
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        t1.start();
+        arrCalculation(a2);
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
 
         long endTime = System.currentTimeMillis();
+
         long output = endTime - startTime;
-        System.out.println(output);
+        System.out.println("Array 2 was completed in "+ output + " milisec." );
     }
+
+    public static void main(String[] args) {
+        arr1(arr);
+        arr2(arr);
+
+    }
+
 }
