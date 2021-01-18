61f6f198408c4c7684b7d2dad2be451c56609b3d
NikFamilyNB <niksipeykin@mail.ru>
2021-01-18 23:01:12 +0300

    create java file lesson6

---
+++ GBLesson6/src/ru/geekBrains/nikSipeykin/lesson6/Server.java
@@ -0,0 +1,14 @@
+package ru.geekBrains.nikSipeykin.lesson6;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    static ServerSocket server;
+    static Socket socket;
+    static final int port = 8189;
+    
+    public static void main(String[] args) {
+
+    }
+}
