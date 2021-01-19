ea8a72939267ba0f97ef58222eddc17b2c134a0a
NikFamilyNB <niksipeykin@mail.ru>
2021-01-19 18:27:39 +0300

    continue work on lesson6

--- GBLesson6/src/ru/geekBrains/nikSipeykin/lesson6/Server.java
+++ GBLesson6/src/ru/geekBrains/nikSipeykin/lesson6/Server.java
@@ -1,14 +1,58 @@
 package ru.geekBrains.nikSipeykin.lesson6;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
+//TODO:
+// 1. Написать консольный вариант клиент\серверного приложения, в котором пользователь может писать сообщения,
+// как на клиентской стороне, так и на серверной. Т.е. если на клиентской стороне написать "Привет", нажать Enter
+// то сообщение должно передаться на сервер и там отпечататься в консоли. Если сделать то же самое на серверной
+// стороне, сообщение соответственно передается клиенту и печатается у него в консоли. Есть одна особенность,
+// которую нужно учитывать: клиент или сервер может написать несколько сообщений подряд, такую ситуацию необходимо
+// корректно обработать
+//    Разобраться с кодом с занятия, он является фундаментом проекта-чата
+//    ВАЖНО! Сервер общается только с одним клиентом, т.е. не нужно запускать цикл, который будет ожидать
+//    второго/третьего/n-го клиентов
+
     static ServerSocket server;
     static Socket socket;
-    static final int port = 8189;
+    static final int PORT = 8189;
     
     public static void main(String[] args) {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+            socket = server.accept();
+            System.out.println("Client connected");
 
+            Scanner scanner = new Scanner(socket.getInputStream());
+
+            while(true){
+                String str = scanner.nextLine();
+
+                if(str.equals("/end")){
+                    System.out.println("Client disconnected");
+                    break;
+                }
+
+                System.out.println("Client: " + str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
