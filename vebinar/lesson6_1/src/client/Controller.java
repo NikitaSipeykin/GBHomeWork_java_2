c20ceacb1e24ea1a498797df64baf7c0d133aacd
NikFamilyNB <niksipeykin@mail.ru>
2021-01-20 17:14:22 +0300

    vc2

--- vebinar/lesson6_1/src/client/Controller.java
+++ vebinar/lesson6_1/src/client/Controller.java
@@ -1,4 +1,17 @@
 package client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+
+
 public class Controller {
+    public TextArea textArea;
+    public TextField textField;
+
+    public void sendMsg(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
